f2591b0758a9f4ec129804f2391899dabe4f23f8
Boris Nikolic <borisnikolic2302@gmail.com>
2023-11-16 23:05:01 +0100

    update

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -2,90 +2,80 @@ import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import "./style.css";
 import dest from "./images/DEST.png";
-import icon from "./Group.svg";
-import purpleTruck from "./images/PurpleTruck.png";
-import news from "./images/Rectangle7.png";
-import profileImage from "./images/Rectangle12.png";
-import journey from "./images/Rectangle11.png";
-import settings from "./images/Rectangle10.png";
-import road from "./images/Rectangle9.png";
-import coverImage from "./images/Cover.png";
+import icon from "./images/Group.svg";
+import purpleTruck from "./images/PURPLETRUCK.png";
+import news from "./images/NEWS.png";
+import profileImage from "./images/PROFILEIMAGE.png";
+import journey from "./images/JOURNEY.png";
+import settings from "./images/SETTINGS.png";
+import road from "./images/ROAD.png";
+import coverImage from "./images/COVERIMAGE.png";
 
 function Home() {
   const navigate = useNavigate();
 
-  const onRectangle2Click = useCallback(() => {
-    navigate("/desktop-2");
+  const onRectangleContainerClick = useCallback(() => {
+    navigate("/travel");
   }, [navigate]);
 
   return (
     <div className="desktop-3">
       <div className="mask-group" />
-      <img className="image-4-icon" alt="" src={coverImage} />
-      <b className="journey-awaits-find-container">
-        <span className="signup-txt">
-          <p className="journey-awaits">Journey Awaits:</p>
-          <p className="journey-awaits"> Find Your Bus Trip</p>
-        </span>
-      </b>
-      <div className="frame-parent">
+      <div className="rectangle-parent">
+        <img className="frame-child" alt="" src={coverImage} />
         <div className="frame-wrapper">
           <div className="group-parent">
             <img className="group-icon" alt="" src={icon} />
             <b className="bustravel">BusTravel</b>
+            <div className="home-parent">
+              <b className="home">HOME</b>
+              <b className="signup">
+                <span className="signup-txt">
+                  <span>SIGN</span>
+                  <span className="up">UP</span>
+                </span>
+              </b>
+            </div>
           </div>
         </div>
-        <div className="home-parent">
-          <b className="home">HOME</b>
-          <b className="signup">
-            <span className="signup-txt">
-              <span>SIGN</span>
-              <span className="up">UP</span>
-            </span>
-          </b>
-        </div>
+        <b className="journey-awaits-find-container">
+          <p className="journey-awaits">Journey Awaits:</p>
+          <p className="journey-awaits"> Find Your Bus Trip</p>
+        </b>
       </div>
-      <img className="desktop-3-item" alt="" src={dest} />
-      <img className="desktop-3-inner" alt="" src={journey} />
-      <img className="rectangle-icon" alt="" src={purpleTruck} />
-      <img className="desktop-3-child1" alt="" src={settings} />
-      <div className="frame-div" />
-      <div className="rectangle-div" />
-      <div className="footer-wrapper">
-        <b className="home">FOOTER</b>
+      <div className="vector-parent">
+        <img className="frame-item" alt="" src={dest} />
+        <b className="destinations">DESTINATIONS</b>
       </div>
-      <img className="desktop-3-child2" alt="" src={news} />
-      <div className="news-wrapper">
-        <b className="home">NEWS</b>
+      <div className="vector-group">
+        <img className="frame-inner" alt="" src={journey} />
+        <b className="your-journey">YOUR JOURNEY</b>
       </div>
-      <img
-        className="desktop-3-child3"
-        alt=""
-        src={road}
-        onClick={onRectangle2Click}
-      />
-      <img className="desktop-3-child4" alt="" src={profileImage} />
-      <div className="frame-group">
-        <div className="frame-wrapper">
+      <div className="vector-container">
+        <img className="rectangle-icon" alt="" src={purpleTruck} />
+        <b className="routes">ROUTES</b>
+      </div>
+      <div className="frame-div">
+        <img className="frame-child1" alt="" src={settings} />
+        <b className="settings">SETTINGS</b>
+      </div>
+      <div className="rectangle-group">
+        <div className="rectangle-div" />
+        <b className="footer">FOOTER</b>
+      </div>
+      <div className="rectangle-container">
+        <img className="frame-child2" alt="" src={news} />
+        <b className="news">NEWS</b>
+      </div>
+      <div className="desktop-3-inner">
+        <div className="vector-parent1" onClick={onRectangleContainerClick}>
+          <img className="component-child" alt="" src={road} />
           <b className="travel">TRAVEL</b>
         </div>
-        <div className="routes-wrapper">
-          <b className="routes">ROUTES</b>
-        </div>
       </div>
-      <div className="frame-container">
-        <div className="frame-wrapper">
-          <b className="profile">PROFILE</b>
-        </div>
-        <div className="settings-wrapper">
-          <b className="profile">SETTINGS</b>
-        </div>
-      </div>
-      <div className="destinations-parent">
-        <b className="home">DESTINATIONS</b>
-        <div className="your-journey-wrapper">
-          <b className="home">YOUR JOURNEY</b>
-        </div>
+      <div className="vector-parent2">
+        <img className="frame-child3" alt="" src={profileImage} />
+        <b className="profile">PROFILE</b>
       </div>
     </div>
   );
