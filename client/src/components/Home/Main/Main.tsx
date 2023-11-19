2961077358a85bb0705f72134ec67130e9f09102
Boris Nikolic <borisnikolic2302@gmail.com>
2023-11-19 17:18:15 +0100

    Login and Register

--- client/src/components/Home/Main/Main.tsx
+++ client/src/components/Home/Main/Main.tsx
@@ -3,9 +3,12 @@ import "./style.css";
 import backgroundVideo from "../assets/pexels_videos_1851190 (2160p).mp4";
 import Travel from "../Travel/Travel";
 import imageForTraveling from "../assets/pexels-pixabay-414916.jpg";
+import DestinationImage from "../Dest/assets/natures-beauty-reflected-tranquil-mountain-waters-generative-ai.jpg";
 import { Parallax } from "react-parallax";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
+import Dest from "../Dest/Dest";
 
 const Main = () => {
   const [selectedOption, setSelectedOption] = useState<string | null>(null);
@@ -13,6 +16,52 @@ const Main = () => {
   const [showRegistrationForm, setShowRegistrationForm] = useState(false);
   const [showLoginForm, setShowLoginForm] = useState(false);
   const travelRef = useRef<HTMLDivElement | null>(null);
+  // Form values
+  // REGISTER VALUES
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleInputRegistrationChange = (name: string, value: string) => {
+    // You can update the state based on the input name
+    switch (name) {
+      case "name":
+        setName(value);
+        break;
+      case "username":
+        setUsername(value);
+        break;
+      case "email":
+        setEmail(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+      case "confirmPassword":
+        setConfirmPassword(value);
+        break;
+      default:
+        break;
+    }
+  };
+  // LOGIN VALUES
+  const [loginEmailOrUsername, setLoginEmailOrUsername] = useState("");
+  const [loginPw, setLoginPw] = useState("");
+  const handleInputLoginChange = (name: string, value: string) => {
+    // You can update the state based on the input name
+    switch (name) {
+      case "loginEmailorUsername":
+        setLoginEmailOrUsername(value);
+        break;
+      case "loginPw":
+        setLoginPw(value);
+        break;
+      default:
+        break;
+    }
+  };
 
   const handleScroll = () => {
     const scrollY = window.scrollY;
@@ -36,18 +85,65 @@ const Main = () => {
     setShowRegistrationForm((prev) => !prev);
     setShowLoginForm(false); // Close login form if open
   };
+  const handleRegisterSubmit = async (ev: any) => {
+    ev.prevent.Default();
+    try {
+      if (password !== confirmPassword) {
+        console.error("Passwords do not match");
+        return;
+      }
+      // Send registration data to backend
+      const response = await axios.post("http://localhost:8081/register", {
+        name: name,
+        username: username,
+        email: email,
+        password: password,
+      });
+
+      // Handle successful registration (you can redirect or show a success message)
+      console.log("Registration successful", response.data);
+    } catch (error: any) {
+      if (error.response && error.response.status === 401) {
+        const errorData = error.response.data;
+        if (errorData.message === "Invalid password") {
+          alert("Invalid password");
+        }
+      } else if (error.request) {
+        alert("User not found");
+        console.log(error.request);
+      } else {
+        console.log("Error", error.message);
+      }
+    }
+  };
 
   const handleLoginClick = () => {
     setShowLoginForm((prev) => !prev);
     setShowRegistrationForm(false); // Close registration form if open
   };
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+  const handleLoginSubmit = async (ev: any) => {
+    ev.preventDefault();
+    try {
+      // Send login data to backend
+      const response = await axios.post("http://localhost:8081/login", {
+        emailOrUsername: loginEmailOrUsername,
+        password: loginPw,
+      });
+
+      // Handle successful login (you can redirect or show a success message)
+      console.log("Login successful", response.data);
+    } catch (error: any) {
+      // Handle login error
+      if (error.response && error.response.data && error.response.data.error) {
+        console.error("Login failed:", error.response.data.error);
+      } else {
+        console.error("Error during login:", error.message);
+      }
+
+      // You can set an error state and display an error message to the user
+    }
+  };
 
   return (
     <div className="container">
@@ -102,25 +198,60 @@ const Main = () => {
             <form>
               <label>
                 Full Name:
-                <input type="name" />
+                <input
+                  type="name"
+                  value={name}
+                  onChange={(e) =>
+                    handleInputRegistrationChange("name", e.target.value)
+                  }
+                />
               </label>{" "}
               <label>
                 Username:
-                <input type="text" />
+                <input
+                  type="name"
+                  value={username}
+                  onChange={(e) =>
+                    handleInputRegistrationChange("username", e.target.value)
+                  }
+                />
               </label>{" "}
               <label>
                 Email
-                <input type="email" />
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) =>
+                    handleInputRegistrationChange("email", e.target.value)
+                  }
+                />
               </label>
               <label>
                 Password:
-                <input type="password" />
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) =>
+                    handleInputRegistrationChange("password", e.target.value)
+                  }
+                />
               </label>
               <label>
                 Confirm Password:
-                <input type="password" />
+                <input
+                  type="password"
+                  value={confirmPassword}
+                  onChange={(e) =>
+                    handleInputRegistrationChange(
+                      "confirmPassword",
+                      e.target.value
+                    )
+                  }
+                />
               </label>
-              <button type="submit">Submit</button>
+              <button onClick={handleRegisterSubmit} type="submit">
+                Submit
+              </button>
             </form>
           </div>
         )}
@@ -139,14 +270,31 @@ const Main = () => {
             </button>
             <form>
               <label>
-                Username:
-                <input type="text" />
+                Email or Username:
+                <input
+                  type="text"
+                  value={loginEmailOrUsername}
+                  onChange={(e) =>
+                    handleInputLoginChange(
+                      "loginEmailorUsername",
+                      e.target.value
+                    )
+                  }
+                />
               </label>
               <label>
                 Password:
-                <input type="password" />
+                <input
+                  type="password"
+                  value={loginPw}
+                  onChange={(e) =>
+                    handleInputLoginChange("loginPw", e.target.value)
+                  }
+                />
               </label>
-              <button type="submit">Submit</button>
+              <button onClick={handleLoginSubmit} type="submit">
+                Submit
+              </button>
             </form>
           </div>
         )}
@@ -165,7 +313,13 @@ const Main = () => {
           </Parallax>
         </div>
       </div>
-      <div></div>
+      <div className="desinations-container">
+        <div className="paralax-content">
+          <Parallax strength={600} bgImage={DestinationImage}>
+            <Dest />
+          </Parallax>
+        </div>
+      </div>
     </div>
   );
 };
--- server/index.js
+++ server/index.js
@@ -1,5 +1,89 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+const jwtSecretKey = crypto.randomBytes(32).toString("hex");
+
+const PORT = process.env.PORT;
 const app = express();
+const pool = require("./db/server");
+
 app.use(express.json());
 app.use(cors());
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World!");
+});
+
+app.post("/register", async (req, res) => {
+  const { name, username, email, password } = req.body;
+
+  if (!name || !username || !email || !password) {
+    return res.status(400).json({ error: "Missing required fields" });
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10); // You can adjust the saltRounds
+
+    // Use the pool to execute the SQL query
+    const result = await pool.query(
+      "INSERT INTO users (name, username, email, password) VALUES ($1, $2, $3, $4) RETURNING *",
+      [name, username, email, hashedPassword]
+    );
+
+    const user = result.rows[0];
+
+    res.status(200).json({ message: "Registration successful", user });
+  } catch (error) {
+    console.error("Error during registration", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { emailOrUsername, password } = req.body;
+
+  if (!emailOrUsername || !password) {
+    return res.status(400).json({ error: "Missing required fields" });
+  }
+
+  try {
+    // Check if the entered value is an email or a username
+    const isEmail = emailOrUsername.includes("@");
+    const key = isEmail ? "email" : "username";
+
+    // Query the database to find the user based on email or username
+    const result = await pool.query(`SELECT * FROM users WHERE ${key} = $1`, [
+      emailOrUsername,
+    ]);
+
+    if (result.rows.length === 0) {
+      // User not found
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    const user = result.rows[0];
+
+    // Use bcrypt.compare to check if the entered password matches the stored hashed password
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      // Incorrect password
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    // Handle successful login (you can redirect or show a success message)
+    console.log("Login successful", user);
+    res.status(200).json({ message: "Login successful", user });
+  } catch (error) {
+    // Handle login error
+    console.error("Error during login", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}.`);
+});
