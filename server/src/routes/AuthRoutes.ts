7af592aeeea157fb99299d21b7faa08592d6f2a8
Boris Nikolic <borisnikolic2302@gmail.com>
2023-12-14 16:00:51 +0100

    koji kurac vise

--- server/src/routes/AuthRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const authRouter = express.Router();
-import { Login, Register } from "../controllers/AuthController";
-import { verifyToken } from "../middleware/VerifyToken";
-authRouter.post("/register", Register);
-authRouter.post("/login", Login);
-
-export { authRouter };
